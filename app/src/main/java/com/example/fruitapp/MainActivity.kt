dea164b9e832f54096662a7183ef089b5d4d33aa
Weixun93 <93048104+Weixun93@users.noreply.github.com>
2025-05-06 20:50:19 +0800

    Fix the show of Main Screen.

--- app/src/main/java/com/example/fruitapp/MainActivity.kt
+++ app/src/main/java/com/example/fruitapp/MainActivity.kt
@@ -7,26 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.fruitapp.ui.theme.FruitAppTheme
-import android.Manifest
-import android.app.Activity
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
-import android.provider.MediaStore
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat
-import androidx.core.content.FileProvider
-import com.example.fruitapp.api.RetrofitClient
-import com.example.fruitapp.api.UploadResponse
 import com.google.android.libraries.places.api.Places
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.MultipartBody
-import okhttp3.RequestBody.Companion.asRequestBody
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import java.io.File
 
 class MainActivity : ComponentActivity() {
 
--- app/src/main/java/com/example/fruitapp/api/RetrofitClient.kt
+++ app/src/main/java/com/example/fruitapp/api/RetrofitClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     private val retrofit = Retrofit.Builder()
-        .baseUrl("https://314b-140-118-110-212.ngrok-free.app")
+        .baseUrl("https://ram-crucial-rarely.ngrok-free.app/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
